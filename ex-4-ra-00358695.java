c6fdb9eaae815dfb436763ce171ed50c64dfe2ce
unknown <maurelio.rosa@gmail.com>
2026-04-12 22:51:02 -0300

    Adiciona exercício 4 - RA 00358695

---
+++ ex-4-ra-00358695.java
@@ -0,0 +1,10 @@
+public class Copa {
+
+    public static void welcomeCopa() {
+        System.out.println("Bem-vindo a Copa 2026!");
+    }
+
+    public static void main(String[] args) {
+        welcomeCopa();
+    }
+}
