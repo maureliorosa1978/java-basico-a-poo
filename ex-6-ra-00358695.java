272a7bae8f7cd1c53637c57395aaed3a45610d92
unknown <maurelio.rosa@gmail.com>
2026-04-12 23:02:44 -0300

    Adiciona exercício 6 - RA 00358695

---
+++ ex-6-ra-00358695.java
@@ -0,0 +1,15 @@
+public class Contador {
+
+    public static void main(String[] args) {
+
+        System.out.println("=============================");
+        System.out.println("  Números de 1 a 10:");
+        System.out.println("=============================");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("  " + i);
+        }
+
+        System.out.println("=============================");
+    }
+}
